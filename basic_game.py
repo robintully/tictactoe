c295a47e7f3225f9467e65f626c7a0e54b1022ba
Robin Tully <mr.robintully@gmail.com>
2016-09-03 18:58:59 -0400

    added winning move checker and fork checker

--- basic_game.py
+++ basic_game.py
@@ -1,25 +1,44 @@
-# Basic Prototype of how Tic Tac Toe works
+board = ["X"," ","X"," "," "," "," "," "," "]
+winning_board_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-board = [" "," "," "," "," "," "," "," "," "]
+
 def print_board():
 	print(board[0:3])
 	print(board[3:6])
 	print(board[6:9])
-	
-def has_player_won(player_board_positions):
-	winning_board_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def cats_game():
+	return " " not in board
+
+# This does not include, mark since player board positions
+def player_has_winning_position(player_board_positions):
 	for winning_combo in winning_board_combinations:
 		if set(winning_combo).issubset(set(player_board_positions)):
 			return True
 	return False
-		
-def player_board_indexes(mark):
+
+def territory_held_by_player(player_mark):
 	player_territory = []
 	for index,board_mark in enumerate(board):
-		if board_mark == mark:
+		if board_mark == player_mark:
 			player_territory.append(index)
 	return player_territory
 	
+def winning_move(index,mark):
+	hypothetical_board = territory_held_by_player(mark) + [index]
+	return player_has_winning_position(hypothetical_board)
+
+def forking_move(index,mark):
+	winningMoves = 0
+	hypothetical_board = territory_held_by_player(mark) + [index]
+	for index in range(0, 9):
+		if player_has_winning_position(hypothetical_board + [index]) and board[index] == " ":
+			winningMoves +=1
+	return winningMoves >= 2
+	
+
+    
+    
 def get_user_move():
 	print("Please enter your desired move location")
 	return int(input())
@@ -28,17 +47,5 @@ def commit_move(index,mark):
 	if board[index] == " ":
 		board[index] = mark
 
-while True:
-	if has_player_won(player_board_indexes("X")):
-		print("X has won!")
-		break
-	if has_player_won(player_board_indexes("O")):
-		print("O has won!")
-		break
-	if " " not in board:
-		print("Cat's Game!")
-		break
-	print_board()
-	commit_move(get_user_move(),"X")
-	commit_move(get_user_move(),"O")
-	
+print_board()
+print(forking_move(4,"X"))
