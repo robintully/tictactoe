81e336208d1d0bcdf107c313973902701730da8b
Robin Tully <mr.robintully@gmail.com>
2016-09-05 00:34:34 -0400

    AI implemented and simple loop for gameplay

--- basic_game.py
+++ basic_game.py
@@ -1,51 +1,83 @@
-board = ["X"," ","X"," "," "," "," "," "," "]
+# X is Human
+# O is Computer
+available_spaces = [0,1,2,3,4,5,6,7,8]
+x_spaces,o_spaces = [],[]
 winning_board_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-
 def print_board():
-	print(board[0:3])
-	print(board[3:6])
-	print(board[6:9])
-
-def cats_game():
-	return " " not in board
-
-# This does not include, mark since player board positions
-def player_has_winning_position(player_board_positions):
-	for winning_combo in winning_board_combinations:
-		if set(winning_combo).issubset(set(player_board_positions)):
-			return True
-	return False
-
-def territory_held_by_player(player_mark):
-	player_territory = []
-	for index,board_mark in enumerate(board):
-		if board_mark == player_mark:
-			player_territory.append(index)
-	return player_territory
+	board_display = [" "," "," "," "," "," "," "," "," "]
+	for space in x_spaces:
+		board_display[space] = "X"
+	for space in o_spaces:
+		board_display[space] = "0"
+	print(board_display[0:3])
+	print(board_display[3:6])
+	print(board_display[6:9])
+	print(" ")
 	
-def winning_move(index,mark):
-	hypothetical_board = territory_held_by_player(mark) + [index]
-	return player_has_winning_position(hypothetical_board)
-
-def forking_move(index,mark):
+def commit_move(players_spaces,index):
+	if index in available_spaces:
+		available_spaces.remove(index)
+		players_spaces.append(index)
+		
+def is_winning_position(players_spaces):
+	for winning_combo in winning_board_combinations:
+		if set(winning_combo).issubset(set(players_spaces)):
+			return True
+			
+def is_forking_move(fork_space,players_spaces):
 	winningMoves = 0
-	hypothetical_board = territory_held_by_player(mark) + [index]
-	for index in range(0, 9):
-		if player_has_winning_position(hypothetical_board + [index]) and board[index] == " ":
+	for space in available_spaces:
+		if is_winning_position(players_spaces + [fork_space] + [space]):
 			winningMoves +=1
 	return winningMoves >= 2
 	
-
-    
-    
+def determine_computer_move():
+	for space in available_spaces:
+		if is_winning_position(o_spaces + [space]):
+			return space
+	for space in available_spaces:
+		if is_winning_position(x_spaces + [space]):
+			return space
+	for space in available_spaces:
+		if is_forking_move(space,o_spaces):
+			return space
+	player_forks = 0
+	for space in available_spaces:
+		if is_forking_move(space,x_spaces):
+			player_forks += 1
+			tempMove = space
+	if player_forks == 1:
+		return tempMove
+	elif player_forks == 2:
+		for space in [1,3,5,7]:
+			if space in available_spaces:
+				return space
+	if 4 in available_spaces:
+		return 4
+	for space in [0,2,6,8]:
+		if space in available_spaces:
+			return space
+	for space in [1,3,5,7]:
+		if space in available_spaces:
+			return space
+			
 def get_user_move():
 	print("Please enter your desired move location")
 	return int(input())
 	
-def commit_move(index,mark):
-	if board[index] == " ":
-		board[index] = mark
+while True:
+	print_board()
+	commit_move(x_spaces,get_user_move())
+	if is_winning_position(x_spaces):
+		print_board()
+		print ("You win!")
+		break
+	commit_move(o_spaces,determine_computer_move())
+	if is_winning_position(o_spaces):
+		print_board()
+		print ("You lose!")
+		break
+	
 
-print_board()
-print(forking_move(4,"X"))
+
